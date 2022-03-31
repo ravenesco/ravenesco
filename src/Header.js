e853341a7ac6975ca05651b773a1cb64d91e41be
Efim Klimov <efim.klimov.95@gmail.com>
2022-03-31 22:41:48 +0300

    Initialize react app

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <h1>Hello</h1>
+        )
+    }
+}
+
+export default Header;
